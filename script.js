372bca58b5901064bb1171c0931c198b5379e96c
kesia almeida <kesia.alves@ymail.com>
2024-01-03 23:38:07 -0300

    edicao da bateria

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
-    updateBatteryLevel(90);
+const chargeLevel = document.getElementById("charge-level");
+const charge = document.getElementById('charge');
+const chargingTimeRef = document.getElementById("charging-time");
 
+window.onload = ( => {
+    if (!navigator.getBattery) {
+        alert("Battery Status Api is Not Supported In Your Browser");
+        return false;
+
+    }
+})
+
+navigator.getBattery().then((battery) => {
+    function updateAllBatteryInfo() {
+        updateChargingInfo();
+        updateLevelInfo();
+    }
+    updateAllBatteryInfo();
+
+
+    battery.addEventListener("chargingchange", () => {
+        updateAllBatteryInfo();
+    });
+
+
+    battery.addEventListener("levelchange", () => {
+        updateAllBatteryInfo();
+    })
+
+    function updateChargingInfo() {
+        if (battery.charging) {
+            charge.classList.add("active");
+            chargingTimeRef.innerText = "";
+        } else {
+            charge.classList.remove("active");
+
+
+            if (parseInt(battery.dischargingTime)) {
+                let hr = parseInt(battery.dischargingTime / 3600);
+                let min = parseInt(battery.dischargingTime / 60 - hr * 60);
+                chargingTimeRef.innerText = `${hr}hr ${min} mins remaining`;
+            }
+        }
+    }
+
+
+    function updateLevelInfo() {
+        let batteryLevel = `${parseInt(battery.level * 100) %}`;
+        charge.style.width = batteryLevel;
+        chargeLevel.textContent = batteryLevel;
+    }
 });
 
-function updateBatteryLevel(level) {
-    const batteryLevelElement = document.getElementById("battery-level");
-    batteryLevelElement.style.width = level + "%" 
-}
+
+
